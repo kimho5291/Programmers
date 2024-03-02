5d6d6a2cff4f5a3db18f441f2fb234dd2b7cf5b4
YoungHo_Kim <48588710+kimho5291@users.noreply.github.com>
2024-03-02 11:05:33 +0900

    Create 1463.js

---
+++ baekjoon/1463.js
@@ -0,0 +1,14 @@
+
+
+let input = `7`.split("\n");
+
+let num = Number(input[0]);
+const DP = new Array(num + 1).fill(0);
+for(let i=2; i<=num; i++) {
+    DP[i] = DP[i-1] + 1;
+    if(i % 2 == 0) DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+    if(i % 3 == 0) DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+}
+console.log(DP[num])
+
+

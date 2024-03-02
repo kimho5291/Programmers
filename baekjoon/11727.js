777bdc1403a7e05e367c42f9eeabc039d033d2ae
YoungHo_Kim <48588710+kimho5291@users.noreply.github.com>
2024-03-02 12:48:31 +0900

    Create 11727.js

---
+++ baekjoon/11727.js
@@ -0,0 +1,17 @@
+
+
+let input = `8`;
+
+let num = +input;
+let dp = Array.from({length: num+1}, () => 0);
+if(num == 1) {
+    console.log(1);
+    return;
+}
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 3;
+for(let i = 3; i <= num; i++) {
+    dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
+}
+console.log(dp[num]);
